9fcaa44d34a44d95c9a6387a227d7f13f7c74660
Chakour Marouane <cmarouan@e1r3p15.1337.ma>
2019-02-14 14:29:08 +0100

    TTTT

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@ echo "external_url 'https://192.168.99.100'" > /etc/gitlab/gitlab.rb
 #echo "nginx['ssl_certificate'] = '/etc/gitlab/ssl/gitlab.crt'" >> /etc/gitlab/gitlab.rb
 #echo "nginx['ssl_certificate_key'] = '/etc/gitlab/ssl/gitlab.key'" >> /etc/gitlab/gitlab.rb
 #echo "gitlab_rails['gitlab_shell_ssh_port'] = 2020" >> /etc/gitlab/gitlab.rb
-#echo "Port 2020" >> /etc/ssh/ssh_config
-#service ssh restart
+echo "Port 2020" >> /etc/ssh/ssh_config
+service ssh restart
 #(/opt/gitlab/embedded/bin/runsvdir-start &) && gitlab-ctl reconfigure
