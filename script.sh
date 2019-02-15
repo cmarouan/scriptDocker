d2254be411498c048f883621c72f0d987752f0ce
Chakour Marouane <cmarouan@e1r3p15.1337.ma>
2019-02-15 14:37:33 +0100

    0000

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
 echo "external_url 'https://192.168.99.100'" > /etc/gitlab/gitlab.rb
+echo "nginx['enable'] = true" >> /etc/gitlab/gitlab.rb
+echo "nginx['redirect_http_to_https'] = true" >> /etc/gitlab/gitlab.rb
+echo "nginx['redirect_http_to_https_port'] = 80" >> /etc/gitlab/gitlab.rb
+echo "nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.crt"" >> /etc/gitlab/gitlab.rb
+echo "nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.key"" >> /etc/gitlab/gitlab.rb
+echo "gitlab_rails['gitlab_shell_ssh_port'] = 2222"  >> /etc/gitlab/gitlab.rb
+echo "Port 2222" >> /etc/ssh/ssh_config
